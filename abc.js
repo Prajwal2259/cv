b23668d48e76da50f715df56dca358b89ad5dd41
Prajwal2259 <81462065+Prajwal2259@users.noreply.github.com>
2021-03-27 14:12:55 +0530

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,19 @@
+
+var subjects=("maths","physics","chemistry");
+var marks=new Array();
+var num1;
+for (var i = 0; i < subjects.length; i++) {
+
+	num1=parseFloat(prompt("enter the marks of the subjects"+subjects[i]));
+	marks[i]=num1;
+}
+for ( m in marks)
+{
+	console.log(marks[m]);
+}
+for( m in marks)
+{
+	console.log(marks[m]);
+	}
+
+	 
